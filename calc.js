f6f276efda5dccc81064162f9cfb67c5615f0828
Onkar Sharma <osharma@epaysystems.com>
2022-03-08 19:22:35 +0530

    Updated logics to work - By: Onkar Sharma

--- calc.js
+++ calc.js
@@ -1,16 +1,25 @@
+$(document).ready(function () {
+	console.log("Onkar Sharma Vatsa")
+})
+
 function button(num) {
-	document.getElementById("txt").value=document.getElementById("txt").value+num;
+	document.getElementById("txt").value = document.getElementById("txt").value + num;
 }
 
-
-function equal() {
+function Equal() {
 	var exp = document.getElementById("txt").value;
-	if(exp){
-	document.getElementById("txt").value = eval(exp)
-}
+	if (exp != null || exp != "") {
+		var result = eval(exp)
+		$("#txt").val(result);
+	}
 }
 
+function Clean() {
+	// document.getElementById("txt").value = "";
+	$("#txt").val('');
+}
 
-function clean() {	
-	document.getElementById("txt").value = "";
+function Back() {
+	var input = $("#txt").val();
+	$("#txt").val(input.substr(0, input.length - 1));
 }
